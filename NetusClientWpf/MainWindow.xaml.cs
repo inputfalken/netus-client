77d7449e27aa3067efba8f341abd667ccb51e03e
Robert Andersson <inputfalken@gmail.com>
2017-02-17 02:18:30 +0100

    Inital commit

---
+++ NetusClientWpf/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace NetusClientWpf {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        private static readonly NetusClient NetusClient = new NetusClient("10.0.2.15", 23000);
+
+        public MainWindow() {
+            InitializeComponent();
+            Loaded += OnLoaded;
+            SendBtn.Click += SendBtnOnClick;
+        }
+
+
+        private async void SendBtnOnClick(object sender, RoutedEventArgs routedEventArgs) {
+            var messageBoxText = MessageBox.Text;
+            await NetusClient.SendMessage(messageBoxText);
+            AddToChatBox(messageBoxText);
+        }
+
+        private async void OnLoaded(object sender, RoutedEventArgs routedEventArgs) {
+            await NetusClient.Connect();
+            AddToChatBox(await NetusClient.ReadMessage());
+        }
+
+        private void AddToChatBox(string message) => ChatBox.Items.Add(message);
+    }
+}
---
+++ NetusClientWpf/NetusClient.cs
@@ -0,0 +1,32 @@
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetusClientWpf {
+    internal class NetusClient {
+        private readonly string _ip;
+        private readonly int _port;
+        private readonly TcpClient _client;
+
+
+        public NetusClient(string ip, int port) {
+            _ip = ip;
+            _port = port;
+            _client = new TcpClient();
+        }
+
+        public async Task Connect() => await _client.ConnectAsync(IPAddress.Parse(_ip), _port);
+
+        public async Task<string> ReadMessage() {
+            return await new StreamReader(_client.GetStream()).ReadLineAsync();
+        }
+
+        public async Task SendMessage(string message) {
+            var buffer = Encoding.ASCII.GetBytes(message + Environment.NewLine);
+            await _client.GetStream().WriteAsync(buffer, 0, buffer.Length);
+        }
+    }
+}
